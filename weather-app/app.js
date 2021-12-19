5edade13eda20570eea7244128af298176a23cfb
Joanna <m.joanna.foss@gmail.com>
2021-12-19 16:24:14 -0600

    notes on asynchronous programming; beginning weather app

---
+++ weather-app/app.js
@@ -0,0 +1,18 @@
+// NOTES: ASYNCHRONOUS PROGRAMMING
+// console.log("starting");
+//
+// setTimeout(() => {
+// 	console.log("2 second timer");
+// }, 2000);
+//
+// setTimeout(()=>{
+// 	console.log("0 second timer");
+// }, 0);
+//
+// console.log("stopping");
+//
+//CONSOLE:
+//starting
+//stopping
+//0 second timer
+//2 second timer
