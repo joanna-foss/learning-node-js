d0f293acc1ca35152ece010b83d34b351986647c
Joanna <m.joanna.foss@gmail.com>
2021-12-21 10:59:49 -0600

    in-prog: added error handling for mapbox and weatherstack

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,3 @@
-// NOTES: ASYNCHRONOUS PROGRAMMING
-// console.log("starting");
-//
-// setTimeout(() => {
-// 	console.log("2 second timer");
-// }, 2000);
-//
-// setTimeout(()=>{
-// 	console.log("0 second timer");
-// }, 0);
-//
-// console.log("stopping");
-//
-//CONSOLE:
-//starting
-//stopping
-//0 second timer
-//2 second timer
-
 const request = require('postman-request');
 const {weatherstackAPIkey, mapboxAPIkey} = require("./keys");
 
@@ -24,10 +5,22 @@ const weatherstackURL = 'http://api.weatherstack.com/current?access_key=' + weat
 const mapboxURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=' + mapboxAPIkey + '&limit=1'
 
 request({url: weatherstackURL, json: true}, (error, response) => {
-	console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees, but it feels like " + response.body.current.feelslike + " degrees.");
+	if (error) {
+		console.log("Unable to retrieve data from weatherstack.");
+	} else if(response.body.error){
+		console.log("Unable to find location.");
+	} else {
+		console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees, but it feels like " + response.body.current.feelslike + " degrees.");
+	}
 });
 
 request({url: mapboxURL, json: true}, (error, response) => {
-	console.log("longitude: " + response.body.features[0].geometry.coordinates[0]);
-	console.log("latitude: " + response.body.features[0].geometry.coordinates[1]);
+	if (error) {
+		console.log("Unable to retrieve data from mapbox.");
+	} else if (response.body.features.length === 0) {
+		console.log("Undefined location.");
+	} else {
+		console.log("longitude: " + response.body.features[0].geometry.coordinates[0]);
+		console.log("latitude: " + response.body.features[0].geometry.coordinates[1]);
+	}
 });
