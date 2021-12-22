690d79e4d79f0e6855df5c9aee9e90c8cd7a9e28
Joanna <m.joanna.foss@gmail.com>
2021-12-22 12:52:03 -0600

    weather app in-prog: added command line prompt for location

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,31 @@
-const {weatherstackAPIkey, mapboxAPIkey} = require("./keys");
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
+const readline = require('readline');
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
-geocode('New Orleans', (error, geocodeData) => {
-	if(error){
-		return console.log('Error: ', error);
-	}
-	forecast(geocodeData.longitude, geocodeData.latitude, (error, forecastData) => {
-		if (error) {
-			return console.log('Error: ', error);
+const askForLocation = () => {
+	rl.question('Please enter in a location: ', (location) => {
+		if (!location) {
+			console.log('No entry detected. Please try again.');
+			return askForLocation();
 		}
-		console.log('Weather Data for ' + geocodeData.location + ':');
-		console.log(forecastData.description);
-		console.log(forecastData.temperature);
-		console.log(forecastData.feels_like);
+		rl.close();
+		geocode(location, (error, geocodeData) => {
+			if (error) {
+				return console.log('Error: ', error);
+			}
+			forecast(geocodeData.longitude, geocodeData.latitude, (error, forecastData) => {
+				if (error) {
+					return console.log('Error: ', error);
+				}
+				console.log('Weather Data for ' + geocodeData.location + ':');
+				console.log(forecastData.description);
+				console.log(forecastData.temperature);
+				console.log(forecastData.feels_like);
+			});
+		});
+
 	});
-});
+}
+
+askForLocation();
