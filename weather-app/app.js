32c5f0e9d22c53a50b920fd39f6b32d58ad60ce6
Joanna <m.joanna.foss@gmail.com>
2021-12-22 11:54:55 -0600

    in-prog: forecast nested in geocode

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,21 +2,17 @@ const {weatherstackAPIkey, mapboxAPIkey} = require("./keys");
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('New Orleans', (error, data) => {
-	console.log('Error', error);
-	console.log('Data', data);
-});
-
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
+geocode('New Orleans', (error, geocodeData) => {
+	if(error){
+		return console.log('Error: ', error);
+	}
+	forecast(geocodeData.longitude, geocodeData.latitude, (error, forecastData) => {
+		if (error) {
+			return console.log('Error: ', error);
+		}
+		console.log('Weather Data for ' + geocodeData.location + ':');
+		console.log(forecastData.description);
+		console.log(forecastData.temperature);
+		console.log(forecastData.feels_like);
+	});
 });
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,18 +2,18 @@ const {weatherstackAPIkey} = require("../keys");
 const request = require("postman-request");
 
 const forecast = (lon, lat, callback) => {
-	const url = 'http://api.weatherstack.com/current?access_key=' + weatherstackAPIkey + '&query=37.8267,-122.4233&units=f';
+	const url = 'http://api.weatherstack.com/current?access_key=' + weatherstackAPIkey + '&query='+ lat + ',' + lon + '&units=f';
 
 	request({url: url, json: true}, (error, response) => {
 		if(error){
-			callback('Unable to connect.', undefined);
+			callback('Unable to connect to weatherstack.', undefined);
 		} else if(response.body.error){
-			callback('Unable to retrieve data.', undefined);
+			callback('Unable to retrieve coordinate data.', undefined);
 		} else {
 			callback(undefined, {
-				description: response.body.current.weather_descriptions[0],
-				temperature: response.body.current.temperature,
-				feels_like: response.body.current.feelslike
+				description: "It is currently " + response.body.current.weather_descriptions[0] + " out.",
+				temperature: "The temperature is " + response.body.current.temperature + " degrees.",
+				feels_like: "And it feels like it is " + response.body.current.feelslike + " degrees out."
 			});
 		}
 	});
