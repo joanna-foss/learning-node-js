a032397fb1f55008d9b45412b3bdc23e191e25d0
Joanna <m.joanna.foss@gmail.com>
2021-12-22 13:38:55 -0600

    completed: es6 objects review

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,40 @@
+//Object property shorthand
+
+const name = 'Joanna';
+const userAge = 300;
+
+// const user = {
+// 	name: name,
+// 	age: userAge,
+// 	location: 'San Antonio'
+// }
+// vvv
+
+const user = {
+	name,
+	age: userAge,
+	location: 'San Antonio'
+}
+
+console.log(user);
+
+// Object destructuring
+
+const product = {
+	label: 'Red notebook',
+	price: 3,
+	stock: 201,
+	salePrice: undefined,
+	rating: 2.5
+}
+
+const {label:productLabel, stock, rating = 5} = product;
+console.log(productLabel);
+console.log(stock);
+console.log(rating);
+
+const transaction = (type, {label, stock}) => {
+	console.log(type, label, stock);
+}
+
+transaction('Order: ', product);
