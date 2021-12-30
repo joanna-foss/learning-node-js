387a0b5e22cf2f424500bb3b00ae1c7186c83958
Joanna <m.joanna.foss@gmail.com>
2021-12-30 17:38:04 -0600

    in-prog: intro into hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,15 +4,30 @@ const express = require('express'); //a function used to call a new express appl
 const app = express();
 const publicDirPath = path.join(__dirname, '../public');
 
-app.use('/', express.static(publicDirPath));
-app.use('/help', express.static(path.join(publicDirPath, '/help.html')));
-app.use('/about', express.static(path.join(publicDirPath, '/about.html')));
+app.set('view engine', 'hbs');
 
-//GOAL: Create two more HTML files
-// 1. Create an html page for about
-// 2. Create an html page for help
-// 3. Remove the old route handlers for both
-// 4. Visit both to test
+app.use('/', express.static(publicDirPath));
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Home Page',
+		name: 'Joanna'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About Page',
+		name: 'Joanna'
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Help Page',
+		name: 'Joanna'
+	});
+});
 
 app.get('/my-weather', (req, res) => {
 	res.send('<h1>The Current Weather</h1>');
