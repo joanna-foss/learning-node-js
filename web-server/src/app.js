5bc85fbd65badf8c0fb71401e704d6db6ac98df5
Joanna <m.joanna.foss@gmail.com>
2021-12-30 21:50:52 -0600

    in-prog: queries

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -35,7 +35,28 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/my-weather', (req, res) => {
-	res.send('<h1>Current Weather</h1>');
+	if(!req.query.address){
+		return res.send({
+			error: 'You must provide an address.'
+		});
+	}
+
+	res.send({
+		location: req.query.address
+	});
+});
+
+app.get('/products', (req, res) => {
+	if(!req.query.search){
+		return res.send({
+			error: 'You must provide a search term.'
+		});
+	}
+
+	console.log(req.query.search);
+	res.send({
+		products: []
+	});
 });
 
 app.get('/help/*', (req, res) => {
